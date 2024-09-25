de64bb90db6b3916b56a25aab05e7193ca61d4a7
Bhawneet Kaur <bhawneet.kaur@incture.com>
2024-09-25 17:12:36 +0530

    lates

---
+++ src/components/atoms/SVGRs/index.js
@@ -0,0 +1,4 @@
+export { default as BackIcon } from './BackIcon';
+export { default as CodeBranch } from './CodeBranch';
+export {default as OpenIssues} from './OpenIssues';
+export {default as Watchers} from './Watchers';
--- src/pages/RepoDetailsPage/RepoDetailsPage.jsx
+++ src/pages/RepoDetailsPage/RepoDetailsPage.jsx
@@ -1,20 +1,28 @@
-import React, { lazy } from 'react';
-import { useParams,Link } from 'react-router-dom';
-import { useFetch } from '../../hooks/useFetch';
-import * as labels from '../../utils/labels';
-import './repoDetailsPage.css';
-import Button from '../../components/atoms/Button/Button';
+import React, { lazy, Suspense } from "react";
+import { useParams, Link } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import * as labels from "../../utils/labels";
+import "./repoDetailsPage.css";
+import Button from "../../components/atoms/Button/Button";
 
-const BackIcon = lazy(() => import('../../components/atoms/SVGRs/BackIcon'));
-const CodeBranch =lazy(()=> import('../../components/atoms/SVGRs/CodeBranch'));
-const Watchers = lazy(()=> import('../../components/atoms/SVGRs/Watchers'));
-const OpenIssues = lazy(()=> import('../../components/atoms/SVGRs/OpenIssues'))
+const BackIcon = lazy(() => import("../../components/atoms/SVGRs/BackIcon"));
+const CodeBranch = lazy(() =>
+  import("../../components/atoms/SVGRs/CodeBranch")
+);
+const Watchers = lazy(() => import("../../components/atoms/SVGRs/Watchers"));
+const OpenIssues = lazy(() =>
+  import("../../components/atoms/SVGRs/OpenIssues")
+);
 
 const RepoDetailsPage = () => {
-  const { id } = useParams(); 
-  const { data: repos, loading, error } = useFetch(process.env.REACT_APP_GITHUB_API);
+  const { id } = useParams();
+  const {
+    data: repos,
+    loading,
+    error,
+  } = useFetch(process.env.REACT_APP_GITHUB_API);
 
-  const repo = repos.find(repo => repo.id === Number(id));
+  const repo = repos.find((repo) => repo.id === Number(id));
 
   if (loading) return <p>{labels.LOADING}</p>;
   if (error) return <p>{labels.LOADING_ERROR}</p>;
@@ -23,24 +31,40 @@ const RepoDetailsPage = () => {
 
   return (
     <section className="repo-details">
+      <Suspense fallback={<div>Loading...</div>}>
         <Link to="/" className="repo-details__back-btn">
-        <BackIcon className="repo-details__back-icon" /> {labels.BACK_TO_REPOS}
-      </Link>
+          <BackIcon className="repo-details__back-icon" />{" "}
+          {labels.BACK_TO_REPOS}
+        </Link>
+      </Suspense>
       <h1 className="repo-details__title">{repo.name || labels.REPO_NAME}</h1>
-      <p className="repo-details__description">{repo.description || labels.REPO_DESCRIPTION}</p>
-      <p className="repo-details__language">{labels.REPO_LANGUAGE}: {repo.language || "N/A"}</p>
+      <p className="repo-details__description">
+        {repo.description || labels.REPO_DESCRIPTION}
+      </p>
+      <p className="repo-details__language">
+        {labels.REPO_LANGUAGE}: {repo.language || "N/A"}
+      </p>
       <div className="repo-details__item">
-          <CodeBranch /> <span>{labels.REPO_FORKS}:</span> {repo.forks}
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <CodeBranch />
+        </Suspense>
+        <span>{labels.REPO_FORKS}:</span> {repo.forks}
+      </div>
 
-        <div className="repo-details__item">
-          <OpenIssues /> <span>{labels.REPO_ISSUES}:</span> {repo.open_issues}
-        </div>
+      <div className="repo-details__item">
+        <Suspense fallback={<div>Loading...</div>}>
+          <OpenIssues />
+        </Suspense>
+        <span>{labels.REPO_ISSUES}:</span> {repo.open_issues}
+      </div>
 
-        <div className="repo-details__item">
-          <Watchers /> <span>{labels.REPO_WATCHERS}:</span> {repo.watchers}
-        </div>
-        <Button
+      <div className="repo-details__item">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Watchers />
+        </Suspense>
+        <span>{labels.REPO_WATCHERS}:</span> {repo.watchers}
+      </div>
+      <Button
         label={labels.REPO_LINK}
         to={repo.html_url}
         external
@@ -51,4 +75,3 @@ const RepoDetailsPage = () => {
 };
 
 export default RepoDetailsPage;
-
